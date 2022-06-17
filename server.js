c2a378dd5b0321391af3d1e4e71902f42488d3d3
Ashley Christman <dev@ashleychristman.com>
2022-06-16 21:55:10 -0400

    Add CRUD methods

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 8000
-require('dotenv').config() 
+require('dotenv').config()
+const { body, validationResult } = require('express-validator')
 
 // DB variables
 let db, 
@@ -26,25 +27,53 @@ app.use(express.json())
 
 // CRUD Methods
 app.get('/', (req, res) => {
-    let contents = db.collection('').find().toArray()
+    db.collection('tasks').find().toArray()
         .then(data => {
-            let taskList = data.map(x => x.title)
+            let taskList = data.map(x => x.name)
             console.log(taskList)
             res.render('index.ejs', {info: taskList})
         })
         .catch(err => console.log(err))
 })
 
-app.post('/api', (req, res) => {
+app.post(
+    '/api', 
+    body('name', 'Task cannot be blank').isLength({ min: 1}), 
+    body('complete').toBoolean(),
+    body('completedCount').toInt(),
+    body('date').toDate(),
+    (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
 
+        console.log(`Post heard`)
+        db.collection('tasks').insertOne(
+            req.body
+        )
+        .then(result => {
+            console.log(result)
+            res.redirect('/')
+        })
+        .catch(error => {
+            console.log(error)
+        })
 })
 
-app.put('updateEntry', (req, res) => {
-
-})
+// app.put('/updateEntry', (req, res) => {
+//     console.log(req.body)
+// })
 
 app.delete('/deleteEntry', (req, res) => {
-
+    db.collection('tasks').deleteOne(
+        {name: req.body.name}
+    )
+    .then(results => {
+        console.log(`Entry deleted`)
+        res.json('Entry deleted')
+    })
+    .catch(error => console.error(error))
 })
 
 // Set up localhost on port
